af67b26913802da52ccbc3f3189799d569f95fe3
Thiago Camargo <thiago.camargo@veon.com>
2018-10-11 11:47:49 +0200

    Include APNS Resposnes

--- src/claws_apns_worker.erl
+++ src/claws_apns_worker.erl
@@ -3,6 +3,8 @@
 
 -behaviour(gen_server).
 
+-include("snatch.hrl").
+
 %% gen_server callbacks
 -export([ init/1,
     handle_call/3,
@@ -20,6 +22,7 @@ handle_call({push, DeviceId, ApnsTopic, Notification}, _From, State) ->
     #{connection_pid := ConnectionPid} = State,
     Headers = #{apns_topic => ApnsTopic},
     Response = apns:push_notification(ConnectionPid, DeviceId, Notification, Headers),
+    snatch:received(Response, #via{jid = DeviceId, exchange = ApnsTopic, claws = claws_apns}),
     error_logger:info_msg("Got APNS response for push[cert]: [~p] for request [~p]", [Response,
         #{connection => ConnectionPid, device => DeviceId, notification => Notification, headers => Headers}]),
     {reply, Response, State};
@@ -28,6 +31,7 @@ handle_call({push_token, Token, DeviceId, ApnsTopic, Notification}, _From, State
     #{connection_pid := ConnectionPid} = State,
     Headers = #{apns_topic => ApnsTopic},
     Response = apns:push_notification_token(ConnectionPid, Token, DeviceId, Notification, Headers),
+    snatch:received(Response, #via{jid = DeviceId, exchange = ApnsTopic, claws = claws_apns}),
     error_logger:info_msg("Got APNS response for push[token]: [~p] for request [~p]", [Response,
         #{token => Token, connection => ConnectionPid, device => DeviceId,
             notification => Notification, headers => Headers}]),
